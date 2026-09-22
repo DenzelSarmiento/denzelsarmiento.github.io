8dae5cbdd655fc0050bbded4fd6bb09ee20e93cc
Denzelcito <denzelosarmiento@gmail.com>
2026-09-21 23:39:05 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: './', // 👈 Fuerza a que busque los estilos en la carpeta actual
+  build: {
+    assetsDir: 'assets', // Agrupa los estilos CSS correctamente
+  }
 })
